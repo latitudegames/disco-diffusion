dc2161c5695f08c16b6ab6bac1d48d5fae1d2ecb
Sean Gillespie <info@spg.business>
2022-03-07 11:16:28 -0500

    Update torch hub and symlink to align with created directories for /models/checkpoints

--- model.py
+++ model.py
@@ -68,9 +68,9 @@ os.symlink('/mnt/models/vgg.pth', os.path.expanduser(
     '~/.cache/taming/modules/autoencoder/lpips/vgg.pth'))
 os.mkdir(os.path.expanduser('/models'))
 os.mkdir(os.path.expanduser('/models/checkpoints'))
-torch.hub.set_dir("models")
+torch.hub.set_dir("/models")
 os.symlink('/mnt/models/vgg16-397923af.pth', os.path.expanduser(
-    'models/checkpoints/vgg16-397923af.pth'))
+    '/models/checkpoints/vgg16-397923af.pth'))
 
 skip_for_run_all = True  # @param {type: 'boolean'}
 
