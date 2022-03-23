d27b5ed22d64af27af0da9bc307f640ccd9d7f76
Sean Gillespie <info@spg.business>
2022-03-23 12:04:23 -0400

    Append AdaBins to sys path in disco_xform_utils.py

--- disco_xform_utils.py
+++ disco_xform_utils.py
@@ -1,10 +1,13 @@
-import torch, torchvision
+import torch
+import torchvision
 import pytorch3d.renderer.cameras as p3dCam
 import midas_utils
 from PIL import Image
 import numpy as np
-import sys, math
+import sys
+import math
 
+sys.path.append('./AdaBins')
 try:
     from infer import InferenceHelper
 except:
@@ -13,11 +16,13 @@ except:
 
 MAX_ADABINS_AREA = 500000
 
+
 @torch.no_grad()
-def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_mat=torch.eye(3).unsqueeze(0), translate=(0.,0.,-0.04), near=2000, far=20000, fov_deg=60, padding_mode='border', sampling_mode='bicubic', midas_weight = 0.3):
+def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_mat=torch.eye(3).unsqueeze(0), translate=(0., 0., -0.04), near=2000, far=20000, fov_deg=60, padding_mode='border', sampling_mode='bicubic', midas_weight=0.3):
     img_pil = Image.open(open(img_filepath, 'rb')).convert('RGB')
     w, h = img_pil.size
-    image_tensor = torchvision.transforms.functional.to_tensor(img_pil).to(device)
+    image_tensor = torchvision.transforms.functional.to_tensor(
+        img_pil).to(device)
 
     use_adabins = midas_weight < 1.0
 
@@ -32,12 +37,15 @@ def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_m
         image_pil_area = w*h
         if image_pil_area > MAX_ADABINS_AREA:
             scale = math.sqrt(MAX_ADABINS_AREA) / math.sqrt(image_pil_area)
-            depth_input = img_pil.resize((int(w*scale), int(h*scale)), Image.LANCZOS) # LANCZOS is supposed to be good for downsampling.
+            # LANCZOS is supposed to be good for downsampling.
+            depth_input = img_pil.resize(
+                (int(w*scale), int(h*scale)), Image.LANCZOS)
         else:
             depth_input = img_pil
         try:
             _, adabins_depth = infer_helper.predict_pil(depth_input)
-            adabins_depth = torchvision.transforms.functional.resize(torch.from_numpy(adabins_depth), image_tensor.shape[-2:], interpolation=torchvision.transforms.functional.InterpolationMode.BICUBIC).squeeze().to(device)
+            adabins_depth = torchvision.transforms.functional.resize(torch.from_numpy(
+                adabins_depth), image_tensor.shape[-2:], interpolation=torchvision.transforms.functional.InterpolationMode.BICUBIC).squeeze().to(device)
             adabins_depth_np = adabins_depth.cpu().numpy()
         except:
             pass
@@ -52,16 +60,16 @@ def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_m
     # MiDaS depth estimation implementation
     print("Running MiDaS depth estimation implementation...")
     sample = torch.from_numpy(img_midas_input).float().to(device).unsqueeze(0)
-    if midas_optimize==True and device == torch.device("cuda"):
-        sample = sample.to(memory_format=torch.channels_last)  
+    if midas_optimize == True and device == torch.device("cuda"):
+        sample = sample.to(memory_format=torch.channels_last)
         sample = sample.half()
     prediction_torch = midas_model.forward(sample)
     prediction_torch = torch.nn.functional.interpolate(
-            prediction_torch.unsqueeze(1),
-            size=img_midas.shape[:2],
-            mode="bicubic",
-            align_corners=False,
-        ).squeeze()
+        prediction_torch.unsqueeze(1),
+        size=img_midas.shape[:2],
+        mode="bicubic",
+        align_corners=False,
+    ).squeeze()
     prediction_np = prediction_torch.clone().cpu().numpy()
 
     print("Finished depth estimation.")
@@ -80,28 +88,38 @@ def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_m
     depth_map = np.expand_dims(depth_map, axis=0)
     depth_tensor = torch.from_numpy(depth_map).squeeze().to(device)
 
-    pixel_aspect = 1.0 # really.. the aspect of an individual pixel! (so usually 1.0)
-    persp_cam_old = p3dCam.FoVPerspectiveCameras(near, far, pixel_aspect, fov=fov_deg, degrees=True, device=device)
-    persp_cam_new = p3dCam.FoVPerspectiveCameras(near, far, pixel_aspect, fov=fov_deg, degrees=True, R=rot_mat, T=torch.tensor([translate]), device=device)
+    # really.. the aspect of an individual pixel! (so usually 1.0)
+    pixel_aspect = 1.0
+    persp_cam_old = p3dCam.FoVPerspectiveCameras(
+        near, far, pixel_aspect, fov=fov_deg, degrees=True, device=device)
+    persp_cam_new = p3dCam.FoVPerspectiveCameras(
+        near, far, pixel_aspect, fov=fov_deg, degrees=True, R=rot_mat, T=torch.tensor([translate]), device=device)
 
     # range of [-1,1] is important to torch grid_sample's padding handling
-    y,x = torch.meshgrid(torch.linspace(-1.,1.,h,dtype=torch.float32,device=device),torch.linspace(-1.,1.,w,dtype=torch.float32,device=device))
+    y, x = torch.meshgrid(torch.linspace(-1., 1., h, dtype=torch.float32, device=device),
+                          torch.linspace(-1., 1., w, dtype=torch.float32, device=device))
     z = torch.as_tensor(depth_tensor, dtype=torch.float32, device=device)
     xyz_old_world = torch.stack((x.flatten(), y.flatten(), z.flatten()), dim=1)
 
     # Transform the points using pytorch3d. With current functionality, this is overkill and prevents it from working on Windows.
     # If you want it to run on Windows (without pytorch3d), then the transforms (and/or perspective if that's separate) can be done pretty easily without it.
-    xyz_old_cam_xy = persp_cam_old.get_full_projection_transform().transform_points(xyz_old_world)[:,0:2]
-    xyz_new_cam_xy = persp_cam_new.get_full_projection_transform().transform_points(xyz_old_world)[:,0:2]
+    xyz_old_cam_xy = persp_cam_old.get_full_projection_transform(
+    ).transform_points(xyz_old_world)[:, 0:2]
+    xyz_new_cam_xy = persp_cam_new.get_full_projection_transform(
+    ).transform_points(xyz_old_world)[:, 0:2]
 
     offset_xy = xyz_new_cam_xy - xyz_old_cam_xy
     # affine_grid theta param expects a batch of 2D mats. Each is 2x3 to do rotation+translation.
-    identity_2d_batch = torch.tensor([[1.,0.,0.],[0.,1.,0.]], device=device).unsqueeze(0)
+    identity_2d_batch = torch.tensor(
+        [[1., 0., 0.], [0., 1., 0.]], device=device).unsqueeze(0)
     # coords_2d will have shape (N,H,W,2).. which is also what grid_sample needs.
-    coords_2d = torch.nn.functional.affine_grid(identity_2d_batch, [1,1,h,w], align_corners=False)
-    offset_coords_2d = coords_2d - torch.reshape(offset_xy, (h,w,2)).unsqueeze(0)
-    new_image = torch.nn.functional.grid_sample(image_tensor.add(1/512 - 0.0001).unsqueeze(0), offset_coords_2d, mode=sampling_mode, padding_mode=padding_mode, align_corners=False)
-    img_pil = torchvision.transforms.ToPILImage()(new_image.squeeze().clamp(0,1.))
+    coords_2d = torch.nn.functional.affine_grid(
+        identity_2d_batch, [1, 1, h, w], align_corners=False)
+    offset_coords_2d = coords_2d - \
+        torch.reshape(offset_xy, (h, w, 2)).unsqueeze(0)
+    new_image = torch.nn.functional.grid_sample(image_tensor.add(1/512 - 0.0001).unsqueeze(
+        0), offset_coords_2d, mode=sampling_mode, padding_mode=padding_mode, align_corners=False)
+    img_pil = torchvision.transforms.ToPILImage()(new_image.squeeze().clamp(0, 1.))
 
     torch.cuda.empty_cache()
 
