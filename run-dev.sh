25f3d93d4ed8f4ad4b489c7f2cebaf85f9c8df18
Sean G <sean@latitude.io>
2022-03-24 20:41:25 +0000

    use inline env variables in run-dev.sh

--- run-dev.sh
+++ run-dev.sh
@@ -1,2 +1,3 @@
+echo $AWS_ACCESS_KEY_ID
 sudo docker build -t latitudetech/disco-diffusion:$1 .
-sudo docker run --rm --shm-size 8G --gpus all --interactive --tty --env-file ./.env -v /mnt/model-storage:/mnt/models --workdir /src latitudetech/disco-diffusion:$1 /bin/bash
+sudo docker run --rm --shm-size 8G --gpus all --interactive --tty -v /mnt/model-storage:/mnt/models --workdir /src -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_REGION=$AWS_REGION -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY latitudetech/disco-diffusion:$1 /bin/bash
