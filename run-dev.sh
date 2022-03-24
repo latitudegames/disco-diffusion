a0533a83dca3c71662430a81ea06b11cc724f779
Sean G <sean@latitude.io>
2022-03-24 20:19:19 +0000

    Update run-dev.sh to reference .env file

--- run-dev.sh
+++ run-dev.sh
@@ -1,2 +1,2 @@
 sudo docker build -t latitudetech/disco-diffusion:$1 .
-sudo docker run --rm --shm-size 8G --gpus all --interactive --tty -v /mnt/model-storage:/mnt/models --workdir /src latitudetech/disco-diffusion:$1 /bin/bash
+sudo docker run --rm --shm-size 8G --gpus all --interactive --tty --env-file ./.env -v /mnt/model-storage:/mnt/models --workdir /src latitudetech/disco-diffusion:$1 /bin/bash
